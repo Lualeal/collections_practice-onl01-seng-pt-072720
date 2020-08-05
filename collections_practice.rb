246c537b49870a49de74c59797fbfa461a652e46
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 20:40:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,15 +3,10 @@ def sort_array_asc(array)
 end 
 
 def sort_array_des(array)
-  array.sort{|a,b| b<=>a}
+  
 end
 
 
-#def sort_array_char_count(array)
- # count = []
-  #array.sort do |count| 
-   # count << 
-    
   def sort_array_char_count(array)
     array.sort do |left, right|
     left.length <=> right.length
